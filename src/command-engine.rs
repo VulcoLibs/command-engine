b5635c2a8bc0799296a833fc74c2c3e79d938af1
FssAy <49795742+FssAy@users.noreply.github.com>
2024-04-25 19:42:30 +0200

    Remove old code

--- src/command-engine.rs
+++ src/command-engine.rs
@@ -1,11 +1,5 @@
 // ToDo: Add tests
 
-#[macro_use] extern crate lazy_static;
-#[cfg(feature = "json")] #[macro_use] extern crate serde;
-
-mod engine;
-pub mod shared;
-
-pub use async_trait::async_trait;
-pub use engine::*;
-pub use shared::{Command, CommandInfo};
+#[cfg(feature = "json")]
+#[macro_use]
+extern crate serde;
